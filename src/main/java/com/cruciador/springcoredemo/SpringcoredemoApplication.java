6fb8a7091129f443cef8ddf01b8b3cc75ee6fe10
Gaurav Maithani <gmaithani2014@gmail.com>
2025-04-27 21:33:57 +0530

    Fix by adding base packages to scan components

--- src/main/java/com/cruciador/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/cruciador/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,7 @@ package com.cruciador.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.cruciador.springcoredemo", "com.cruciador.util"})
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
