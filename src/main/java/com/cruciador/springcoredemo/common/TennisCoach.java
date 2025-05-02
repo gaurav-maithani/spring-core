4d2d5d0735aa34fbad0e8a60e9902fc605c4de18
Gaurav Maithani <gmaithani2014@gmail.com>
2025-05-02 22:21:17 +0530

    Add Constructor logs and show Lazy annotation

--- src/main/java/com/cruciador/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/cruciador/springcoredemo/common/TennisCoach.java
@@ -1,12 +1,17 @@
 package com.cruciador.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Lazy
 public class TennisCoach implements Coach {
 
+    public TennisCoach () {
+        System.out.println("In constructor :: " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "practise your backhend volley";
--- src/main/java/com/cruciador/springcoredemo/rest/DemoController.java
+++ src/main/java/com/cruciador/springcoredemo/rest/DemoController.java
@@ -15,6 +15,8 @@ public class DemoController {
     @Autowired
     public DemoController(Coach myCoach) { //constructor injection
         this.myCoach = myCoach;
+        System.out.println("In constructor :: " + getClass().getSimpleName());
+
     }
 
     @GetMapping("/dailyworkout")
